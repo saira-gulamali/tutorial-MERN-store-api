a0eaf048c1b7aa7b7b2b9ae304045b94dd7e7172
saira-gulamali <sguacamole98@gmail.com>
2024-06-11 14:01:35 +0100

    add basic routes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
 const app = express();
 
@@ -16,6 +17,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route</a>');
 });
+
+app.use("/api/v1/products", productsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
