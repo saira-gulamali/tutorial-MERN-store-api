00d96f4e2e425f24be2638515e787a33f9f3ee65
saira-gulamali <sguacamole98@gmail.com>
2024-06-11 13:43:15 +0100

    add basic express server and db config

---
+++ app.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+// async errors
+
+const express = require("express");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const connectDB = require("./db/connect");
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// routes
+
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route</a>');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    // connectDB
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
