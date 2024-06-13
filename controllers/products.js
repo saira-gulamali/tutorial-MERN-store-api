d50aacf72a4ab819d006826f9384275418dad9c2
saira-gulamali <sguacamole98@gmail.com>
2024-06-13 16:56:40 +0100

    add sort, filter, pagination, numeric filter functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,91 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
   // throw new Error("testing async errors");
-  res.status(200).json({ msg: "products testing route" });
+  // const search = "al";
+  // const products = await Product.find({
+  //   name: { $regex: search, $options: "i" },
+  // });
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const products = await Product.find({ price: { $gt: 30, $lt: 60 } })
+    .sort("price")
+    .select("name price")
+    .limit(limit)
+    .skip(skip);
+
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  console.log(req.query);
+
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  if (numericFilters) {
+    console.log(req.query);
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|<=|>=|=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    console.log(filters);
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = {
+          ...queryObject[field],
+          [operator]: Number(value),
+        };
+      }
+    });
+  }
+
+  console.log({ queryObject });
+
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("-createdAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
